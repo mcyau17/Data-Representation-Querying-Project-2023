b6e6e6849b71bcb979178a01883b74ad23811a1b
mcyau17 <tommyyau76@gmail.com>
2023-12-18 16:38:21 +0000

    Created the react app

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
